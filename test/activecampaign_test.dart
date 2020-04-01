fb91f7efb242e8e7f807695bb7058b11783659a1
duc <duc@bookbotkids.com>
2020-04-01 23:46:06 +0700

    Update test document

--- test/activecampaign_test.dart
+++ test/activecampaign_test.dart
@@ -7,12 +7,20 @@ void main() {
     final configs = {
       "activeCampaignAccount": "YOUR_ACTIVE_CAMPAIGN_ACCOUNT",
       "activeCampaignKey": "YOUR_ACTIVE_CAMPAIGN_KEY",
+      "activeCampaignEventKey": "YOUR_ACTIVE_CAMPAIGN_EVENT_KEY",
+      "activeCampaignEventActid": "YOUR_ACTIVE_CAMPAIGN_EVENT_ACT_ID",
       "proxyUrl": "YOUR_PROXY_SERVER" // optional, to bypass CORs in Flutter web
     };
 
     ActiveCampaign.config(configs);
+    // add tag
     var result = await ActiveCampaign.shared.addTagToContact(
         "your_email@domain.com", "first name", "last name", "your tag");
     expect(true, result != null);
+
+    // tracking
+    var trackingResult = await ActiveCampaign.shared.trackEvent(
+        'event_name', 'your_email@domain.com', {'your_property': 'your_data'});
+    expect(true, trackingResult != null);
   });
 }
